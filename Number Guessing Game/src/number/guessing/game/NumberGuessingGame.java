ce6ae7909338d334190717381a176c3a3e683c57
Sushmita Hubli <hublisushmita7@gmail.com>
2023-03-22 23:13:14 +0530

    first commit

---
+++ Number Guessing Game/src/number/guessing/game/NumberGuessingGame.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package number.guessing.game;
+
+/**
+ *
+ * @author HP
+ */
+public class NumberGuessingGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    //    new Generate.setVisible(true);
+        
+    }
+    
+}
